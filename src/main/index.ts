062869b3dfbb1b7b5f11577761754def84e9ae08
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-08-19 10:43:21 +0900

    electron 최대, 최소 사이즈 수정

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,12 @@ import icon from "../../resources/icon.png?asset";
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    maxWidth: 1920,
+    maxHeight: 1080,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === "linux" ? { icon } : {}),

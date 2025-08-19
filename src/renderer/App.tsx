b26392a2fc9426ff0178f1b9f608af2cb5f2ac5d
justn-hyeok <justn.hyeok@gmail.com>
2025-08-19 10:25:50 +0900

    refactor: App 컴포넌트 리팩터

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,15 +2,16 @@ import { useState } from "react";
 import { MainFrame, Sidebar } from "./components";
 import "./assets/styles/base.css";
 import "./assets/styles/fonts.css";
-import "./App.css";
+import styles from "./App.module.css";
 
 export type PageType = "새 대화" | "앱 연결" | "할 일" | "new";
 
 function App(): React.JSX.Element {
   const [currentPage, setCurrentPage] = useState<PageType>("새 대화");
 
+
   return (
-    <div className="app">
+    <div className={styles.app}>
       <Sidebar currentPage={currentPage} setCurrentPage={setCurrentPage} />
       <MainFrame currentPage={currentPage} />
     </div>
